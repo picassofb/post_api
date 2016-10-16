c4b4c568e70c945522bfe5c7818c33fbdb6cfe4c
Ed <picassofb@hotmail.com>
2016-10-16 11:53:21 -0500

    Api url / view

---
+++ src/posts/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+from posts.models import Post
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'slug',
+            'content',
+        ]
